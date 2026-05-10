f77a0df35765983fb60430bf7f113a3c68442fcb
shivam86010 <94790147+shivam86010@users.noreply.github.com>
2026-05-10 23:40:30 +0530

    apply lazy loading+suspense

--- frontend/src/routers/Index.tsx
+++ frontend/src/routers/Index.tsx
@@ -1,40 +1,58 @@
+import  { Suspense, lazy } from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
-import Login from "../pages/Login"; 
-import DashboardLayout from '../layout/Index'
-import NotFound from "../pages/NotFound";
-import Case from '../pages/Cases'
-import CaseDashboard from '../pages/CaseDashboard'
-import CaseDetailView from '../components/caseEvidence/CaseDetailView'
+
+const Login = lazy(() => import("../pages/Login"));
+const DashboardLayout = lazy(() => import("../layout/Index"));
+const NotFound = lazy(() => import("../pages/NotFound"));
+const Case = lazy(() => import("../pages/Cases"));
+const CaseDashboard = lazy(() => import("../pages/CaseDashboard"));
+const CaseDetailView = lazy(
+  () => import("../components/caseEvidence/CaseDetailView"),
+);
 
 const Index = () => {
-  const isAuthenticated = true; 
+  const isAuthenticated = true;
 
   return (
-    <Routes>
-      {/* Root Route */}
-      <Route 
-        path="/" 
-        element={
-          isAuthenticated ? <Navigate to="/dashboard" replace /> : <Login />
-        } 
-      />
+    <Suspense
+      fallback={
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: "20px",
+            fontWeight: "bold",
+          }}
+        >
+          Loading...
+        </div>
+      }
+    >
+      <Routes>
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? <Navigate to="/dashboard" replace /> : <Login />
+          }
+        />
 
-      {/* Dashboard Routes */}
-      <Route 
-        path="/dashboard" 
-        element={
-          isAuthenticated ? <DashboardLayout /> : <Navigate to="/" replace />
-        }
-      >
-        <Route path="case" element={<Case />} />
-        <Route path="cases/view/:id" element={<CaseDetailView />} />
-        <Route index element={<CaseDashboard />} />
-      </Route>
+        <Route
+          path="/dashboard"
+          element={
+            isAuthenticated ? <DashboardLayout /> : <Navigate to="/" replace />
+          }
+        >
+          <Route path="case" element={<Case />} />
+          <Route path="cases/view/:id" element={<CaseDetailView />} />
+          <Route index element={<CaseDashboard />} />
+        </Route>
 
-      {/* 404 */}
-      <Route path="*" element={<NotFound />} />
-    </Routes>
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
   );
-}
+};
 
 export default Index;
